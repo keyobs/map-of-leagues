e90a12575d3388a40009248cb7a10de40de31be1
keyobs <elodie.koob@affilae.com>
2023-02-13 01:03:30 +0100

    aliases + eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,13 +7,18 @@ module.exports = {
     'extends': [
         'eslint:recommended',
         'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
         'plugin:@typescript-eslint/recommended',
+        'plugin:react/jsx-runtime',
         'prettier'
     ],
     'overrides': [
     ],
     'parser': '@typescript-eslint/parser',
     'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
         'ecmaVersion': 'latest',
         'sourceType': 'module'
     },
@@ -22,21 +27,16 @@ module.exports = {
         '@typescript-eslint'
     ],
     'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
+        'indent': [ 'error', 4 ],
+        'camelcase': 'error',
+        "no-multi-assign": "error",
+        "eqeqeq": [ "warn", "smart" ],
+        "no-var": "warn",
+        "prefer-const": "warn",
+        "no-duplicate-imports": "warn",
+        'linebreak-style': [ 'warn','unix' ],
+        'quotes': [ 'warn','single' ],
+        'semi': [ 'warn','always' ],
+        'react/prop-types': 'off',
     }
 };
