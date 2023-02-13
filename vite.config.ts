e9b2379b3b7389c3df7ef847448283c8793c7611
keyobs <elodie.koob@affilae.com>
2023-02-13 11:44:17 +0100

    preferred alias config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import * as path from 'path';
 
+const projectRootDir = path.resolve(__dirname);
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     resolve: {
-        alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+        alias: {
+            '@assets': path.resolve(projectRootDir, './src/assets'),
+            '@components': path.resolve(projectRootDir, './src/components'),
+            '@pages': path.resolve(projectRootDir, './src/pages'),
+        },
     },
 });
