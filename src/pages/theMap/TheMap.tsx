5dcf72f30135154c84b5be3b10e01bb058618f29
keyobs <elodie.koob@affilae.com>
2023-02-14 17:28:42 +0100

    extract Map + display markers from mocks

--- src/pages/theMap/TheMap.tsx
+++ src/pages/theMap/TheMap.tsx
@@ -1,24 +1,31 @@
 import './theMap.less';
 import { t } from 'i18next';
 
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import { leagueData } from '@templates/mocks';
 
 const TheMapPage = () => {
     return (
         <div className="theMap">
-            <div className="leafletContainer">
-                <MapContainer
-                    center={[47.4, 7.7]}
-                    zoom={5}
-                    scrollWheelZoom={false}
-                >
-                    <TileLayer
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    />
-                </MapContainer>
-            </div>
+            <Map />
         </div>
     );
 };
 export default TheMapPage;
+
+const Map = () => {
+    return (
+        <div className="leafletContainer">
+            <MapContainer center={[47.4, 7.7]} zoom={5} scrollWheelZoom={false}>
+                {[...leagueData].map((league) => (
+                    <Marker key={league.id} position={league.coordinates} />
+                ))}
+
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+            </MapContainer>
+        </div>
+    );
+};
