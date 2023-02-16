39f6f4f90cc587451d1486ff4bb2569a231d2b88
keyobs <elodie.koob@affilae.com>
2023-02-16 12:51:36 +0100

    Button add a league + drawer

--- src/pages/theMap/TheMap.tsx
+++ src/pages/theMap/TheMap.tsx
@@ -1,31 +1,82 @@
 import './theMap.less';
 import { t } from 'i18next';
 
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { leagueData } from '@templates/mocks';
+import { useState } from 'react';
+import { Button, Drawer } from '@mui/material';
 
 const TheMapPage = () => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const onClose = () => setIsOpen(false);
+    const addALeague = () => {
+        setIsOpen(true);
+    };
+
     return (
         <div className="theMap">
-            <Map />
+            <div className="cockpit">
+                <Cockpit addALeague={addALeague} />
+            </div>
+            <div className="leafletContainer">
+                <Map />
+            </div>
+            <CreateEditLeagueDrawer isOpen={isOpen} onClose={onClose} />
         </div>
     );
 };
 export default TheMapPage;
 
-const Map = () => {
+interface TCockpit {
+    addALeague: () => void;
+}
+const Cockpit = (props: TCockpit) => {
+    const { addALeague } = props;
     return (
-        <div className="leafletContainer">
-            <MapContainer center={[47.4, 7.7]} zoom={5} scrollWheelZoom={false}>
-                {[...leagueData].map((league) => (
-                    <Marker key={league.id} position={league.coordinates} />
-                ))}
+        <Button variant="contained" onClick={() => addALeague()}>
+            ADD A LEAGUE
+        </Button>
+    );
+};
 
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                />
-            </MapContainer>
-        </div>
+const Map = () => {
+    const [activeMark, setActiveMark] = useState<string | null>(null);
+    console.log(activeMark);
+
+    return (
+        <MapContainer center={[47.4, 7.7]} zoom={5} scrollWheelZoom={false}>
+            {[...leagueData].map((league) => (
+                <Marker
+                    key={league.id}
+                    position={league.coordinates}
+                    eventHandlers={{
+                        click: () => {
+                            setActiveMark(league.id);
+                        },
+                    }}
+                >
+                    <Popup>{league.name}</Popup>
+                </Marker>
+            ))}
+
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+        </MapContainer>
+    );
+};
+
+interface TCreateEditLeagueDrawer {
+    isOpen: boolean;
+    onClose: () => void;
+}
+const CreateEditLeagueDrawer = (props: TCreateEditLeagueDrawer) => {
+    const { isOpen, onClose } = props;
+
+    return (
+        <Drawer open={isOpen} anchor="right" onClose={onClose}>
+            <div>Hop hop</div>
+        </Drawer>
     );
 };
