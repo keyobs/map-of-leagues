00478dc621a6c414351a6c4c347b8d28cb30dd67
keyobs <elodie.koob@affilae.com>
2023-06-19 14:12:37 +0200

    Extract Drawer create

---
+++ src/components/DrawerCreateEditLeague.tsx
@@ -0,0 +1,126 @@
+import Drawer from '@mui/material/Drawer';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
+import { useState, useEffect } from 'react';
+import { t } from 'i18next';
+import { useQuery } from 'react-query';
+
+import {
+    TCityAutocompletePayload,
+    getCityAutocomplete,
+} from '@api/geoapify/getCityAutocomplete';
+
+type TEvent = React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>;
+
+interface TCreateEditLeagueDrawer {
+    isOpen: boolean;
+    onClose: () => void;
+}
+const DrawerCreateEditLeague = (props: TCreateEditLeagueDrawer) => {
+    const { isOpen, onClose } = props;
+
+    const [leagueName, setLeagueName] = useState<string>('');
+    const [leagueCity, setLeagueCity] = useState<TCitiesAutocompleteOption>({
+        placeId: '',
+        label: '',
+    });
+
+    const onChangeCityLabel = (newValue: string | null) => {
+        newValue === null
+            ? setLeagueCity({ placeId: '', label: '' })
+            : setLeagueCity((state) => ({ ...state, ['label']: newValue }));
+    };
+
+    const onChooseCityOption = (
+        newOption: TCitiesAutocompleteOption | null
+    ) => {
+        newOption === null
+            ? setLeagueCity({ placeId: '', label: '' })
+            : setLeagueCity(() => newOption);
+    };
+
+    const onSearchCity = (args: TCityAutocompletePayload) => {
+        return getCityAutocomplete(args);
+    };
+
+    const citiesQuery = useQuery(
+        ['cities', leagueCity],
+        () => onSearchCity({ text: leagueCity.label }),
+        {
+            enabled: false,
+        }
+    );
+
+    useEffect(() => {
+        if (leagueCity.placeId === '' && leagueCity.label !== '')
+            citiesQuery.refetch();
+    }, [leagueCity]);
+
+    type TCitiesAutocompleteOption = {
+        placeId: string;
+        label: string;
+    };
+    const cityAutocompleteOptions = (): TCitiesAutocompleteOption[] => {
+        if (citiesQuery.data) {
+            return citiesQuery.data.results.map((matchingResult) => ({
+                placeId: matchingResult.place_id,
+                label: matchingResult.formatted,
+            }));
+        }
+        return [];
+    };
+
+    return (
+        <Drawer
+            ModalProps={{ disableScrollLock: false }}
+            open={isOpen}
+            anchor="left"
+            onClose={onClose}
+            BackdropProps={{ invisible: true }}
+        >
+            <div className="leagueDrawer">
+                <header>{t('league_form_title')}</header>
+
+                <form>
+                    <TextField
+                        id="name"
+                        className="textField"
+                        label={t('league_form_field_label_name')}
+                        value={leagueName}
+                        onChange={(event: TEvent) =>
+                            setLeagueName(event.target.value)
+                        }
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+
+                    <Autocomplete
+                        id="city_autocomplete"
+                        options={cityAutocompleteOptions()}
+                        getOptionLabel={(option) => option.label}
+                        value={leagueCity}
+                        isOptionEqualToValue={(option, value) =>
+                            option.placeId === value.placeId
+                        }
+                        onInputChange={(event, newValue) =>
+                            onChangeCityLabel(newValue)
+                        }
+                        onChange={(event, newValue) =>
+                            onChooseCityOption(newValue)
+                        }
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label={t('league_form_field_label_city')}
+                            />
+                        )}
+                    />
+                </form>
+            </div>
+        </Drawer>
+    );
+};
+
+export default DrawerCreateEditLeague;
--- src/pages/theMap/TheMap.tsx
+++ src/pages/theMap/TheMap.tsx
@@ -1,22 +1,12 @@
 import './theMap.less';
 import Button from '@mui/material/Button';
-import Drawer from '@mui/material/Drawer';
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
 
 import { useState, useEffect } from 'react';
 import { t } from 'i18next';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { useQuery } from 'react-query';
 
 import { leagueData } from '@templates/mocks';
-
-import {
-    TCityAutocompletePayload,
-    getCityAutocomplete,
-} from '@api/geoapify/getCityAutocomplete';
-
-type TEvent = React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>;
+import DrawerCreateEditLeague from '@components/DrawerCreateEditLeague';
 
 const TheMapPage = () => {
     const [isOpen, setIsOpen] = useState<boolean>(true);
@@ -35,7 +25,7 @@ const TheMapPage = () => {
             <div className="leafletContainer">
                 <Map />
             </div>
-            <CreateEditLeagueDrawer isOpen={isOpen} onClose={onClose} />
+            <DrawerCreateEditLeague isOpen={isOpen} onClose={onClose} />
         </div>
     );
 };
@@ -77,115 +67,5 @@ const Map = () => {
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
         </MapContainer>
-    );
-};
-
-interface TCreateEditLeagueDrawer {
-    isOpen: boolean;
-    onClose: () => void;
-}
-const CreateEditLeagueDrawer = (props: TCreateEditLeagueDrawer) => {
-    const { isOpen, onClose } = props;
-
-    const [leagueName, setLeagueName] = useState<string>('');
-    const [leagueCity, setLeagueCity] = useState<TCitiesAutocompleteOption>({
-        placeId: '',
-        label: '',
-    });
-
-    const onChangeCityLabel = (newValue: string | null) => {
-        newValue === null
-            ? setLeagueCity({ placeId: '', label: '' })
-            : setLeagueCity((state) => ({ ...state, ['label']: newValue }));
-    };
-
-    const onChooseCityOption = (
-        newOption: TCitiesAutocompleteOption | null
-    ) => {
-        newOption === null
-            ? setLeagueCity({ placeId: '', label: '' })
-            : setLeagueCity(() => newOption);
-    };
-
-    const onSearchCity = (args: TCityAutocompletePayload) => {
-        return getCityAutocomplete(args);
-    };
-
-    const citiesQuery = useQuery(
-        ['cities', leagueCity],
-        () => onSearchCity({ text: leagueCity.label }),
-        {
-            enabled: false,
-        }
-    );
-
-    useEffect(() => {
-        if (leagueCity.placeId === '' && leagueCity.label !== '')
-            citiesQuery.refetch();
-    }, [leagueCity]);
-
-    type TCitiesAutocompleteOption = {
-        placeId: string;
-        label: string;
-    };
-    const cityAutocompleteOptions = (): TCitiesAutocompleteOption[] => {
-        if (citiesQuery.data) {
-            return citiesQuery.data.results.map((matchingResult) => ({
-                placeId: matchingResult.place_id,
-                label: matchingResult.formatted,
-            }));
-        }
-        return [];
-    };
-
-    return (
-        <Drawer
-            ModalProps={{ disableScrollLock: false }}
-            open={isOpen}
-            anchor="left"
-            onClose={onClose}
-            BackdropProps={{ invisible: true }}
-        >
-            <div className="leagueDrawer">
-                <header>{t('league_form_title')}</header>
-
-                <form>
-                    <TextField
-                        id="name"
-                        className="textField"
-                        label={t('league_form_field_label_name')}
-                        value={leagueName}
-                        onChange={(event: TEvent) =>
-                            setLeagueName(event.target.value)
-                        }
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-
-                    <Autocomplete
-                        id="city_autocomplete"
-                        options={cityAutocompleteOptions()}
-                        getOptionLabel={(option) => option.label}
-                        value={leagueCity}
-                        isOptionEqualToValue={(option, value) =>
-                            option.placeId === value.placeId
-                        }
-                        onInputChange={(event, newValue) =>
-                            onChangeCityLabel(newValue)
-                        }
-                        onChange={(event, newValue) =>
-                            onChooseCityOption(newValue)
-                        }
-                        renderInput={(params) => (
-                            <TextField
-                                {...params}
-                                label={t('league_form_field_label_city')}
-                            />
-                        )}
-                    />
-                </form>
-            </div>
-        </Drawer>
     );
 };
