134eb5946b15d4f9dbc27759606457f95f2e2baa
keyhup <e.koob@younup.fr>
2023-10-05 15:59:15 +0200

    change: change TLeague type

--- src/templates/mocks.ts
+++ src/templates/mocks.ts
@@ -1,15 +1,16 @@
 export type TLeague = {
     id: string;
     name: string;
-    coordinates: [number, number];
+    lat: number;
+    lon: number;
 };
 
 export const leagueData: TLeague[] = [
-    { id: '0001', name: 'RD Nice', coordinates: [43.710175, 7.261953] },
-    { id: '0002', name: 'RD Bordeaux', coordinates: [44.837788, -0.57918] },
-    { id: '0003', name: 'RD Munich', coordinates: [48.135124, 11.581981] },
-    { id: '0004', name: 'RD Brest', coordinates: [48.38987, -4.48718] },
-    { id: '0005', name: 'RD Madrid', coordinates: [40.4167, -3.7167] },
-    { id: '0006', name: 'RD Hull', coordinates: [42.2861, -70.8835] },
-    { id: '0007', name: 'RD Bergame', coordinates: [45.695, 9.67] },
+    {id: '0001', name: 'RD Nice', lat: 43.710175, lon: 7.261953},
+    {id: '0002', name: 'RD Bordeaux', lat: 44.837788, lon: -0.57918},
+    {id: '0003', name: 'RD Munich', lat: 48.135124, lon: 11.581981},
+    {id: '0004', name: 'RD Brest', lat: 48.38987, lon: -4.48718},
+    {id: '0005', name: 'RD Madrid', lat: 40.4167, lon: -3.7167},
+    {id: '0006', name: 'RD Hull', lat: 42.2861, lon: -70.8835},
+    {id: '0007', name: 'RD Bergame', lat: 45.695, lon: 9.67}
 ];
